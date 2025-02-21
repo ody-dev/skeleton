c1ccce6a15086472384098762ee43c1b57fe0719
ilyas <deckersilyas@gmail.com>
2025-02-21 23:47:06 +0100

    Add routes

--- App/routes.php
+++ App/routes.php
@@ -3,5 +3,8 @@ declare(strict_types=1);
 
 use Ody\Core\Facades\Route;
 
-Route::get('/test', '\App\Http\Controllers\PostController:test');
+Route::get('/users', '\App\Http\Controllers\PostController:index');
+Route::get('/users/{id}', '\App\Http\Controllers\PostController:find');
+Route::post('/users', '\App\Http\Controllers\PostController:create');
+Route::put('/users/{id}', '\App\Http\Controllers\PostController:update');
 
