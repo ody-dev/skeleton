7719e1945ddbaeea06fc3e4c369475c876ebecc6
ilyas <deckersilyas@gmail.com>
2025-02-22 23:00:06 +0100

    Add ExampleCommand.php

---
+++ App/Console/Commands/ExampleCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Ody\Core\Console\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExampleCommand extends Command
+{
+    protected string $commandName = 'example:command';
+    protected string $commandDescription = "A command";
+
+    protected function configure()
+    {
+        $this
+            ->setName($this->commandName)
+            ->setDescription($this->commandDescription);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return true;
+    }
+}
