57226efd61c849eacbe3f0ae7b425c50709580d0
ilyas <deckersilyas@gmail.com>
2025-02-22 23:01:31 +0100

    update database config

--- bin/index.php
+++ bin/index.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 use Ody\Core\Env;
 use Ody\Core\Facades\Facade;
-use Ody\DB\Eloquent;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -12,7 +11,7 @@ Env::load(__DIR__ . '/../');
 $app = \Ody\Core\DI\Bridge::create();
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
-$app->addErrorMiddleware((bool) $_ENV['DEBUG'], (bool) $_ENV['DEBUG'], (bool) $_ENV['DEBUG']);
+$app->addErrorMiddleware((bool) $_ENV['APP_DEBUG'], (bool) $_ENV['APP_DEBUG'], (bool) $_ENV['APP_DEBUG']);
 Facade::setFacadeApplication($app);
 
 /**
@@ -23,4 +22,6 @@ require __DIR__ . '/../App/routes.php';
 /**
  * Register DB
  */
-Eloquent::boot(require __DIR__ . '/../config/database.php');
+if (class_exists('Ody\DB\Eloquent')) {
+    \Ody\DB\Eloquent::boot(config('database')['environments'][$_ENV['APP_ENV']]);
+}
--- config/database.php
+++ config/database.php
@@ -1,13 +1,32 @@
 <?php
 
 return [
-    'driver'    => 'mysql',
-    'host'      => $_ENV['DB_HOST'],
-    'database'  => $_ENV['DB_DATABASE'],
-    'username'  => $_ENV['DB_USERNAME'],
-    'password'  => $_ENV['DB_PASSWORD'],
-    'port'      => $_ENV['DB_PORT'],
-    'charset'   => 'utf8',
-    'collation' => 'utf8_general_ci',
-    'prefix'    => ''
+    'migration_dirs' => [
+        'migrations' => 'database/migrations',
+    ],
+    'environments' => [
+        'local' => [
+            'adapter' => 'mysql',
+            'host' => $_ENV['DB_HOST'],
+            'port' => $_ENV['DB_PORT'], // optional
+            'username' => $_ENV['DB_USERNAME'],
+            'password' => $_ENV['DB_PASSWORD'],
+            'db_name' => $_ENV['DB_DATABASE'],
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci', // optional, if not set default collation for utf8mb4 is used
+            'prefix'    => ''
+        ],
+        'production' => [
+            'adapter' => 'mysql',
+            'host' => 'production_host',
+            'port' => 3306, // optional
+            'username' => 'user',
+            'password' => 'pass',
+            'db_name' => 'my_production_db',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci', // optional, if not set default collation for utf8mb4 is used
+        ],
+    ],
+    'default_environment' => 'local',
+    'log_table_name' => 'phoenix_log',
 ];
