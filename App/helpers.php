e76cd9a156d89203d0f523b0226b1110c4362e71
ilyas <deckersilyas@gmail.com>
2025-02-23 23:08:20 +0100

    Refactor server start

--- App/helpers.php
+++ App/helpers.php
@@ -1,8 +1,1 @@
 <?php
-
-if (! function_exists('config')) {
-    function config(string $key)
-    {
-        return require __DIR__ . "/../config/{$key}.php";
-    }
-}
--- bin/index.php
+++ bin/index.php
@@ -6,22 +6,22 @@ use Ody\Core\Facades\Facade;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-Env::load(__DIR__ . '/../');
-
-$app = \Ody\Core\DI\Bridge::create();
-$app->addBodyParsingMiddleware();
-$app->addRoutingMiddleware();
-$app->addErrorMiddleware((bool) $_ENV['APP_DEBUG'], (bool) $_ENV['APP_DEBUG'], (bool) $_ENV['APP_DEBUG']);
-Facade::setFacadeApplication($app);
-
-/**
- * Register routes
- */
-require __DIR__ . '/../App/routes.php';
-
-/**
- * Register DB
- */
-if (class_exists('Ody\DB\Eloquent')) {
-    \Ody\DB\Eloquent::boot(config('database')['environments'][$_ENV['APP_ENV']]);
-}
+//Env::load(__DIR__ . '/../');
+//
+//$app = \Ody\Core\DI\Bridge::create();
+//$app->addBodyParsingMiddleware();
+//$app->addRoutingMiddleware();
+//$app->addErrorMiddleware((bool) $_ENV['APP_DEBUG'], (bool) $_ENV['APP_DEBUG'], (bool) $_ENV['APP_DEBUG']);
+//Facade::setFacadeApplication($app);
+//
+///**
+// * Register routes
+// */
+//require __DIR__ . '/../App/routes.php';
+//
+///**
+// * Register DB
+// */
+//if (class_exists('Ody\DB\Eloquent')) {
+//    \Ody\DB\Eloquent::boot(config('database')['environments'][$_ENV['APP_ENV']]);
+//}
