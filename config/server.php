30ee05150b3f0bd55e21e6f9fbb8fad06995c720
Iliad <deckersilyas@gmail.com>
2025-02-23 23:23:02 +0100

    Update server.php

--- config/server.php
+++ config/server.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-    /*
-     * mode
-     * SWOOLE_BASE ,
-     * SWOOLE_PROCESS
-     */
     'mode' => SWOOLE_BASE,
     'host' => '127.0.0.1',
     'port' => 9501 ,
@@ -23,10 +18,6 @@ return [
          */
         'log_level' => SWOOLE_LOG_DEBUG ,
         'log_file' => storagePath('logs/ody.log') ,
-    
-        /*
-        This key causes Fomo to receive a complete HTTP data packet and prevents it from receiving incomplete HTTP packets.
-        */
         'open_http_protocol' => true,
     ],
 
@@ -36,19 +27,11 @@ return [
     ] ,
 
     /*
-     * The following services are created for better performance in the program, only one object is created from them and they can be used throughout the program
+     * The following services are created for better performance 
+     * in the program, only one object is created from them and 
+     * they can be used throughout the program
      */
-//    'services' => [
-//        Fomo\Services\Auth::class ,
-//        Fomo\Services\Cache::class ,
-//        Fomo\Services\Database::class ,
-//        Fomo\Services\Elasticsearch::class ,
-//        Fomo\Services\Language::class ,
-//        Fomo\Services\Mail::class ,
-//        Fomo\Services\Redis::class ,
-//        Fomo\Services\Response::class ,
-//        Fomo\Services\Validation::class ,
-//    ] ,
+    'services' => [] ,
 
     /*
      * Files and folders that must be changed in real time
@@ -59,26 +42,5 @@ return [
         'database',
         'composer.lock',
         '.env',
-    ] ,
-
-    /*
-     * Each of the following causes changes to the performance of the desired class. (so be careful in using them)
-     */
-    'advanceMode' => [
-        /*
-         * advanced mode in Fomo\Request\Request class
-         *
-         * By activating the advanced mode in this class, you can access the data you want in an advanced way
-         * For example, consider that the user has sent you a array of the information of several customers.
-         * If the advanced mode is not active, you can only access an array of all customer information
-         *
-         * For example, the:
-         * $request->get('customers')
-         *
-         * But if the advanced mode is active, you can access any data you need from customers
-         * For example, the:
-         * $request->get('customers.*.name')
-         */
-        'request' => DISABLE
-    ]
+    ] 
 ];
