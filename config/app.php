f0a14749cbad1e700a9226f5b12ace82f7d0a619
Ilyas Deckers <deckersilyas@gmail.com>
2025-02-27 15:14:19 +0100

    update config files

--- config/app.php
+++ config/app.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-    'debug' => env('APP_DEBUG' , false)
+    'debug' => env('APP_DEBUG' , false),
+    /*
+     * The following services are created for better performance
+     * in the program, only one object is created from them and
+     * they can be used throughout the program
+     */
+    'services' => [],
 ];
